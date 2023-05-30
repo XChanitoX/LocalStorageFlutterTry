c58765ec4682ef5c0f4d6ea33b4e64b7236d1fb7
XChanitoX <christian.salazar.a@utec.edu.pe>
2023-05-29 22:19:59 -0500

    Finally Done

--- pruebalocalstorage/lib/views/login_page.dart
+++ pruebalocalstorage/lib/views/login_page.dart
@@ -1,10 +1,11 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:permission_handler/permission_handler.dart';
+//import 'package:permission_handler/permission_handler.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:image_gallery_saver/image_gallery_saver.dart';
+//import 'package:image_gallery_saver/image_gallery_saver.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart' as path;
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -14,107 +15,61 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  //bool loading = false;
-  //double progress = 0.0;
-  //final Dio dio = Dio();
+  File _imageFile = File('');
+  bool _fotoTomada = false;
 
-  Future<bool> saveFile(File foto, String fileName) async {
-    Directory? directory;
+  Future<void> _takePhoto() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: ImageSource.camera);
 
-    try {
+    if (pickedFile != null) {
+      setState(() {
+        _imageFile = File(pickedFile.path);
+      });
+
       if (Platform.isAndroid) {
-        if (await _requestPermission(Permission.storage)) {
-          directory = await getExternalStorageDirectory();
-          String newPath = "";
-          List<String> folders = directory!.path.split("/");
-          for (int x = 1; x < folders.length; x++) {
-            String folder = folders[x];
-            if (folder != "Android") {
-              newPath += '/$folder';
-            } else {
-              break;
-            }
-          }
-          newPath = '$newPath/DCIM/GallosVideos';
-          directory = Directory(newPath);
-        } else {
-          return false;
-        }
-      } else {
-        if (await _requestPermission(Permission.photos)) {
-          directory = await getTemporaryDirectory();
-        } else {
-          return false;
-        }
-      }
-
-      directory.create(recursive: true);
-      if (!await directory.exists()) {
-        await directory.create(recursive: true);
-      }
-
-      if (await directory.exists()) {
-        File saveFile = File("${directory.path}/$fileName");
-        await saveFile.writeAsBytes(foto.readAsBytesSync());
-
-        if (Platform.isAndroid) {
-          await ImageGallerySaver.saveFile(saveFile.path);
-        } else {
-          await ImageGallerySaver.saveFile(saveFile.path,
-              isReturnPathOfIOS: true);
-        }
-        return true;
-      }
-      return false;
-    } catch (e) {
-      return false;
-    }
-  }
-
-  Future<bool> _requestPermission(Permission permission) async {
-    if (await permission.isGranted) {
-      return true;
-    } else {
-      var result = await permission.request();
-      if (result == PermissionStatus.granted) {
-        return true;
-      } else {
-        return false;
+        await saveImageToGallery(_imageFile);
       }
     }
   }
 
-  Future<File> getImage() async {
-    final ImagePicker picker = ImagePicker();
+  Future<void> saveImageToGallery(File imageFile) async {
+    final directory = await getExternalStorageDirectory();
+    var newPath = "";
+    List<String> paths = directory!.path.split("/");
+    for (int x = 1; x < paths.length; x++) {
+      String folder = paths[x];
+      if (folder != "Android") {
+        newPath += "/$folder";
+      } else {
+        break;
+      }
+    }
 
-    final XFile? image = await picker.pickImage(source: ImageSource.camera);
-
-    // Obtener la ruta de la imagen capturada
-    final String picturePath = image!.path;
-
-    // Crear un objeto File a partir de la ruta
-    final File picture = File(picturePath);
-
-    return picture;
+    final imagePath =
+        path.join(newPath, 'DCIM', 'CriaderosCompetitivos', 'Imagenes');
+    await Directory(imagePath).create(recursive: true);
+    final fileName = path.basename(imageFile.path);
+    await imageFile.copy('$imagePath/$fileName').then((value) => setState(() {
+          _fotoTomada = true;
+        }));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: ButtonBar(
-        alignment: MainAxisAlignment.center,
-        children: [
-          ElevatedButton(
-            onPressed: () async {
-              File foto = await getImage();
-              String fileName = "gallito.jpg";
-              saveFile(foto, fileName);
-            },
-            child: const Text('Download Video'),
-          )
-        ],
+      appBar: AppBar(
+        title: const Text('Tomar foto y guardar localmente'),
       ),
-    ));
+      body: Center(
+        child: _fotoTomada
+            ? Image.file(_imageFile)
+            : const Text('No se ha tomado ninguna foto'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _takePhoto,
+        child: const Icon(Icons.camera_alt),
+      ),
+    );
   }
 }
